93e39d0e61b0ff761132c60fcd8ee55bb01869a6
Herdina <gherdina7@gmail.com>
2022-08-21 20:43:03 -0300

    fix: nullish values on parameter

--- src/utils/capitalize.ts
+++ src/utils/capitalize.ts
@@ -1,4 +1,4 @@
-export default function capitalize(word: string) {
+export default function capitalize(word: string = '') {
   const capitalized = word.split('')
     .map((letter, index) => (index === 0 ? letter.toUpperCase() : letter));
   return capitalized.join('');

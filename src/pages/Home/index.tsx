d3aa3d8ddcf97076156c29fd4f8b1260153a6de8
Giovane <giovanedaniell@gmail.com>
2022-08-20 22:10:29 -0300

    refactor: moved requests to the Home component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,22 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
+import { PokemonSpecies } from '../../interfaces/Generation';
+import PokemonsService from '../../services/PokemonsService';
 
-const Home: FC = () => <h1>Página Home</h1>;
+const Home: FC = () => {
+  const [pokemons, setPokemons] = useState<PokemonSpecies[]>([]);
+
+  async function fetchGeneration() {
+    const pokemonsResponse = await PokemonsService.getByGeneration(1).then(
+      (data) => data.pokemon_species,
+    );
+    setPokemons(pokemonsResponse);
+  }
+
+  useEffect(() => {
+    fetchGeneration();
+  }, []);
+
+  return <h1>Página Home</h1>;
+};
 
 export default Home;

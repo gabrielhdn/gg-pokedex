fb2981a7d34786e4693055b0f1e241c2e59dfd20
Giovane <giovanedaniell@gmail.com>
2022-08-20 23:16:29 -0300

    refactor: fixed the request on Home component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,13 @@
 import { FC, useEffect, useState } from 'react';
-import { PokemonSpecies } from '../../interfaces/Generation';
 import PokemonsService from '../../services/PokemonsService';
 import PokemonCard from '../../components/PokemonCard';
 
 const Home: FC = () => {
-  const [pokemons, setPokemons] = useState<PokemonSpecies[]>([]);
+  const [pokemons, setPokemons] = useState<any[]>([]);
 
   async function fetchGeneration() {
-    const pokemonsResponse = await PokemonsService.getByGeneration(1).then(
-      (data) => data.pokemon_species,
+    const pokemonsResponse = await PokemonsService.getByGeneration('first').then(
+      (data) => data.results,
     );
     setPokemons(pokemonsResponse);
   }

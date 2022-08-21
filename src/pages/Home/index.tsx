9258f4069995630c41f66dcb854ccce00cb677b8
Giovane <giovanedaniell@gmail.com>
2022-08-20 23:26:22 -0300

    feat(Home): create home styled container

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,15 @@
 import { FC, useEffect, useState } from 'react';
 import PokemonsService from '../../services/PokemonsService';
 import PokemonCard from '../../components/PokemonCard';
+import * as S from './style';
 
 const Home: FC = () => {
   const [pokemons, setPokemons] = useState<any[]>([]);
 
   async function fetchGeneration() {
-    const pokemonsResponse = await PokemonsService.getByGeneration('first').then(
-      (data) => data.results,
-    );
+    const pokemonsResponse = await PokemonsService.getByGeneration(
+      'first',
+    ).then((data) => data.results);
     setPokemons(pokemonsResponse);
   }
 
@@ -17,11 +18,11 @@ const Home: FC = () => {
   }, []);
 
   return (
-    <>
+    <S.HomeContainer>
       {pokemons.map(({ name }) => (
         <PokemonCard name={name} />
       ))}
-    </>
+    </S.HomeContainer>
   );
 };
 

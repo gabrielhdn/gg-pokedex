fad7418b7c3003da43fe0ef87d283bcb5ab50f58
Giovane <giovanedaniell@gmail.com>
2022-08-20 22:22:50 -0300

    feat(services) created getByName method on PokemonsService class

--- src/services/PokemonsService.ts
+++ src/services/PokemonsService.ts
@@ -2,9 +2,17 @@ import axios from 'axios';
 import { IGeneration } from '../interfaces/Generation';
 
 class PokemonService {
-  public getByGeneration(generation: number) {
+  baseUrl: string = 'https://pokeapi.co/api/v2';
+
+  public async getByGeneration(generation: number) {
     return axios
-      .get(`https://pokeapi.co/api/v2/generation/${generation}/`)
+      .get(`${this.baseUrl}/generation/${generation}/`)
+      .then(({ data }) => data);
+  }
+
+  public async getByName(name: string) {
+    return axios
+      .get(`${this.baseUrl}/pokemon/${name}`)
       .then(({ data }) => data);
   }
 }

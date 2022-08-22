04662b061e615066088b5668ee9ed8459a110080
Herdina <gherdina7@gmail.com>
2022-08-21 21:12:12 -0300

    feat: pokemon colors in object literals

---
+++ src/styles/pokemonTypeColors.ts
@@ -0,0 +1,41 @@
+export interface IPokemonTypeColors {
+  fire: string
+  grass: string
+  poison: string
+  water: string
+  flying: string
+  bug: string
+  normal: string
+  electric: string
+  ground: string
+  fairy: string
+  fighting: string
+  psychic: string
+  rock: string
+  steel: string
+  ice: string
+  ghost: string
+  dragon: string
+  dark: string
+}
+
+export const PokemonTypeColors = {
+  fire: '#F77D25',
+  grass: '#9BCC50',
+  poison: '#B97FC9',
+  water: '#4592C4',
+  flying: '#9DE7FC',
+  bug: '#729F3F',
+  normal: '#A4ACAF',
+  electric: '#EED535',
+  ground: '#AB9842',
+  fairy: '#FDB9E9',
+  fighting: '#D46622',
+  psychic: '#F366B9',
+  rock: '#A38C21',
+  steel: '#9EB7B8',
+  ice: '#51C4E7',
+  ghost: '#7B62A3',
+  dragon: '#F16E57',
+  dark: '#707070',
+};

98631e04974a73ac98dbf3bb006fa9bc003d4f9b
Giovane <giovanedaniell@gmail.com>
2022-08-22 22:02:39 -0300

    style: changed background of the home page

--- src/pages/Home/style.ts
+++ src/pages/Home/style.ts
@@ -1,6 +1,12 @@
+/* eslint-disable import/prefer-default-export */
 import styled from 'styled-components';
+import dayBackground from '../../assets/images/b.png';
 
 export const HomeContainer = styled.main`
+  background: url(${dayBackground});
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
   width: 100vw;
   height: 100vh;
   display: flex;
